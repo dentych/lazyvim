b1c51ca37eb89e92f223549b4ed25cf10f639843
dentych <dennis@tychsen.me>
2023-07-21 18:24:49 +0200

    Add initial files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dentych LazyVim config
+
+Setup LazyVim configured for my dev environments.
+
+## Requirements
+
+* NerdFont "JetBrainsMono Nerd Font" (https://www.nerdfonts.com/font-downloads)
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      autoformat = true,
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+          nls.builtins.formatting.fish_indent,
+          nls.builtins.diagnostics.fish,
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.formatting.goimports,
+          -- nls.builtins.diagnostics.flake8,
+        },
+      }
+    end,
+  },
+}
