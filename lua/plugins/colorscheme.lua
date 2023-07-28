533e71f3492139ebb2cb767b8091cb04bb8e31e9
dentych <dennis@tychsen.me>
2023-07-28 21:39:25 +0200

    Comment white

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
             on_highlights = function(hl, c)
                 hl.LineNr = { fg = c.green }
                 hl.CursorLineNr = { fg = c.orange }
+                hl.Comment = { fg = "#cccccc" }
             end,
         },
     },
