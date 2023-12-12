8edbdab0e286c7d91fc1b5a0a7140a669ec5b06d
dentych <dennis@tychsen.me>
2023-12-12 13:14:27 +0100

    Make highlight bg color brighter

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,7 @@ return {
                 hl.CursorLineNr = { fg = c.orange }
                 hl.Comment = { fg = "#cccccc" }
                 hl.WinSeparator = { fg = "#cccccc" }
+                hl.Visual = { bg = "#60637f" }
             end,
         },
     },
